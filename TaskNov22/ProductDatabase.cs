df443e1b9d488b846974c4dff210c32b5a3755e8
AliSeyfullazadaa <116652745+AliSeyfullazadaa@users.noreply.github.com>
2022-11-22 13:44:55 +0400

    Add files via upload

---
+++ TaskNov22/ProductDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNov22
+{
+    internal class ProductDatabase
+    {
+        public Product[] Products=new Product[5];
+        public int a=0;
+
+        public void AddProduct(Product product)
+        {
+            if (Products[Products.Length-1]==null)
+            {
+                for(int i=0;i<Products.Length;i++)
+                {
+                    if(Products[i]==null)
+                    {
+                        Products[i]=product;
+                        a = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The database is full.");
+            }
+        }
+        
+        public void SearchProduct(Product Product[])
+        {
+
+        }
+
+
+    }
+}
---
+++ TaskNov22/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskNov22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
